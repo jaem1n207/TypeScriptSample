333d73a00e0e3214d82d269ea1f1a8705ae84629
jaem1n207 <jaem1n207@naver.com>
2020-03-12 01:55:04 +0900

    [ADD] basic function

--- ts-practice/src/practice.ts
+++ ts-practice/src/practice.ts
@@ -1,28 +1,15 @@
-let count = 0; // 숫자
-count += 1;
-count = "갑자기 분위기 문자열"; // error 발생
-
-const message: string = "hello world"; // 문자열
-
-const done: boolean = true; // 불리언 값
-
-const numbers: number[] = [1, 2, 3]; // 숫자 배열
-const messages: string[] = ["hello", "world"]; // 문자열 배열
-
-messages.push(1); // 에러 발생
-
-let mightBeUndefined: string | undefined = undefined; // string 일수도 있고 undefined 일수도
-let nullableNumber: number | null = null; // number 일수도 null 일수도
-
-let color: "red" | "orange" | "yellow" = "red"; // red, orange, yellow 중 하나
-color = "yellow";
-color = "green"; // 에러 발생
-
-enum Color {
-  Red = 1,
-  Green = 2,
-  Blue = 4
+function sum(x: number, y: number): number {
+  return x * y;
 }
-let c: Color = Color.Green;
-let colorName: string = Color[2];
-console.log(colorName);
+
+sum(1, 2);
+
+function sumArray(numbers: number[]): number {
+  return numbers.reduce((acc, current) => acc + current, 0);
+}
+
+const total = sumArray([1, 2, 3, 4, 5]);
+
+function returnNothing(): void {
+  console.log("I am just saying hello world.");
+}
