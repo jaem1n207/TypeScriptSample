a653b4c847bfc42b6ce3916cf0479ecc5d72b1df
jaem1n207 <jaem1n207@naver.com>
2020-03-12 02:25:05 +0900

    [ADD] class interface

--- ts-practice/dist/practice.js
+++ ts-practice/dist/practice.js
@@ -1,12 +1,32 @@
 "use strict";
-function sum(x, y) {
-    return x * y;
-}
-sum(1, 2);
-function sumArray(numbers) {
-    return numbers.reduce(function (acc, current) { return acc + current; }, 0);
-}
-var total = sumArray([1, 2, 3, 4, 5]);
-function returnNothing() {
-    console.log("I am just saying hello world.");
-}
+var Circle = /** @class */ (function () {
+    // `implements` 키워드를 사용하여 해당 클래스가 Shape interface 의 조건을 충족하겠다는 것을 명시한다.
+    function Circle(radius) {
+        this.radius = radius;
+        this.radius = radius;
+    }
+    // 너비를 가져오는 함수 구현
+    Circle.prototype.getArea = function () {
+        return this.radius * this.radius * Math.PI;
+    };
+    return Circle;
+}());
+var Rectangle = /** @class */ (function () {
+    function Rectangle(width, height) {
+        this.width = width;
+        this.height = height;
+        this.width = width;
+        this.height = height;
+    }
+    Rectangle.prototype.getArea = function () {
+        return this.width * this.height;
+    };
+    return Rectangle;
+}());
+var circle = new Circle(5);
+var rectangle = new Rectangle(10, 5);
+console.log(circle.radius);
+var shapes = [new Circle(5), new Rectangle(10, 5)];
+shapes.forEach(function (shape) {
+    console.log(shape.getArea());
+});
--- ts-practice/src/practice.ts
+++ ts-practice/src/practice.ts
@@ -1,15 +1,37 @@
-function sum(x: number, y: number): number {
-  return x * y;
+// Shape 라는 interface 선언
+interface Shape {
+  getArea(): number; // Shape interface 에는 getArea 라는 함수가 꼭 있어야 하며 해당 함수의 반환값은 숫자이다.
 }
 
-sum(1, 2);
+class Circle implements Shape {
+  // `implements` 키워드를 사용하여 해당 클래스가 Shape interface 의 조건을 충족하겠다는 것을 명시한다.
 
-function sumArray(numbers: number[]): number {
-  return numbers.reduce((acc, current) => acc + current, 0);
+  constructor(public radius: number) {
+    this.radius = radius;
+  }
+
+  // 너비를 가져오는 함수 구현
+  getArea() {
+    return this.radius * this.radius * Math.PI;
+  }
 }
 
-const total = sumArray([1, 2, 3, 4, 5]);
-
-function returnNothing(): void {
-  console.log("I am just saying hello world.");
+class Rectangle implements Shape {
+  constructor(private width: number, private height: number) {
+    this.width = width;
+    this.height = height;
+  }
+  getArea() {
+    return this.width * this.height;
+  }
 }
+
+const circle = new Circle(5);
+const rectangle = new Rectangle(10, 5);
+console.log(circle.radius);
+
+const shapes: Shape[] = [new Circle(5), new Rectangle(10, 5)];
+
+shapes.forEach(shape => {
+  console.log(shape.getArea());
+});
