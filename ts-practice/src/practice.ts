3f3890b04a99d6037d5c3dc596b993e4bd4bcc8e
jaem1n207 <jaem1n207@naver.com>
2020-03-12 02:40:00 +0900

    [UPDATE] extends

--- ts-practice/src/practice.ts
+++ ts-practice/src/practice.ts
@@ -1,37 +1,20 @@
-// Shape 라는 interface 선언
-interface Shape {
-  getArea(): number; // Shape interface 에는 getArea 라는 함수가 꼭 있어야 하며 해당 함수의 반환값은 숫자이다.
+interface Person {
+  name: string;
+  age?: number;
 }
 
-class Circle implements Shape {
-  // `implements` 키워드를 사용하여 해당 클래스가 Shape interface 의 조건을 충족하겠다는 것을 명시한다.
-
-  constructor(public radius: number) {
-    this.radius = radius;
-  }
-
-  // 너비를 가져오는 함수 구현
-  getArea() {
-    return this.radius * this.radius * Math.PI;
-  }
+interface Developer extends Person {
+  skills: string[];
 }
 
-class Rectangle implements Shape {
-  constructor(private width: number, private height: number) {
-    this.width = width;
-    this.height = height;
-  }
-  getArea() {
-    return this.width * this.height;
-  }
-}
+const person: Person = {
+  name: "짱구",
+  age: 20
+};
 
-const circle = new Circle(5);
-const rectangle = new Rectangle(10, 5);
-console.log(circle.radius);
+const expert: Developer = {
+  name: "도라에몽",
+  skills: ["javascript", "react"]
+};
 
-const shapes: Shape[] = [new Circle(5), new Rectangle(10, 5)];
-
-shapes.forEach(shape => {
-  console.log(shape.getArea());
-});
+const people: Person[] = [person, expert];
