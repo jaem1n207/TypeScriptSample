371634e0de7b3f90319d5a56d624e2a2b8b2f2ff
jaem1n207 <jaem1n207@naver.com>
2020-03-13 02:53:31 +0900

    [ADD] GreetingsComponent

---
+++ ts-react-tutorial/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Greetings from "./Greetings";
+
+function App() {
+  const onClick = (name: string) => {
+    console.log(`${name} says hello`);
+  };
+  return <Greetings name="React" onClick={onClick} />;
+}
+
+export default App;
