f09a4e5d3d7141ac949a4472b5475f20ad0fd7d4
jaem1n207 <jaem1n207@naver.com>
2020-03-13 22:36:47 +0900

    [ADD] ReducerSample with reducer

--- ts-react-tutorial/src/App.tsx
+++ ts-react-tutorial/src/App.tsx
@@ -2,16 +2,17 @@ import React from "react";
 
 // import Counter from "./Counter";
 // import Greetings from "./Greetings";
-import MyFrom from "./MyForm";
+// import MyFrom from "./MyForm";
+import ReducerSample from "./ReducerSample";
 
 function App() {
   /* const onClick = (name: string) => {
     console.log(`${name} says hello`);
   }; */
-  const onSubmit = (form: { name: string; description: string }) => {
+  /* const onSubmit = (form: { name: string; description: string }) => {
     console.log(form);
-  };
-  return <MyFrom onSubmit={onSubmit} />;
+  }; */
+  return <ReducerSample />;
 }
 
 export default App;
