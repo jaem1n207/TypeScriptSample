be5b0ef01e4e166bbb810b224d43517e388ae608
jaem1n207 <jaem1n207@naver.com>
2020-03-14 23:38:14 +0900

    [UPDATE] TodosContext (Create TodosProvider)

--- ts-context-api-tutorial/src/contexts/TodosContext.tsx
+++ ts-context-api-tutorial/src/contexts/TodosContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, Dispatch } from "react";
+import { createContext, Dispatch, useReducer } from "react";
 
 export type Todo = {
   id: number;
@@ -38,4 +38,36 @@ function todosReducer(state: TodosState, action: Action): TodosState {
     default:
       throw new Error("Unhandled action");
   }
+}
+
+export function TodosContextProvider({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  const [todos, dispatch] = useReducer(todosReducer, [
+    {
+      id: 1,
+      text: "Context API 배우기",
+      done: true
+    },
+    {
+      id: 2,
+      text: "TypeScript 배우기",
+      done: true
+    },
+    {
+      id: 3,
+      text: "TypeScript와 Context API 함께 사용하기",
+      done: false
+    }
+  ]);
+
+  return (
+    <TodosDispatchContext.Provider value={dispatch}>
+      <TodosStateContext.Provider value={todos}>
+        {children}
+      </TodosStateContext.Provider>
+    </TodosDispatchContext.Provider>
+  );
 }
