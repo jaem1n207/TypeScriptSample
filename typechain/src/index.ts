828585f726a0a16c09d9e6e3aa247f3fefce5e63
jaem1n207 <jaem1n207@naver.com>
2020-07-23 11:51:14 +0900

    Add: static func

--- typechain/src/index.ts
+++ typechain/src/index.ts
@@ -1,20 +1,43 @@
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+import * as CryptoJS from "crypto-js";
+
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+
+  static calculaterBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string => CryptoJS.SHA256(index + previousHash + timestamp + data) + "";
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-const jaemin = new Human("Jaemin", 19, "male");
+const genesisBlock: Block = new Block(0, "02020202002", "", "Hello", 123456);
 
-const sayHi = (person: Human): string => {
-  return `hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
-};
+let blockChain: Block[] = [genesisBlock];
 
-console.log(sayHi(jaemin));
+const getBlockchain = (): Block[] => blockChain;
+
+const getLatestBlock = (): Block => blockChain[blockChain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+console.log(blockChain);
 
 export {};
