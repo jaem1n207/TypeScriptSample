bc6a2a079adf81a2b05079f2ac45b3da516fc6d9
jaem1n207 <jaem1n207@naver.com>
2020-07-23 13:38:27 +0900

    Add: validateStructure & CheckBlock

--- typechain/src/index.ts
+++ typechain/src/index.ts
@@ -1,18 +1,27 @@
 import * as CryptoJS from "crypto-js";
 
 class Block {
-  public index: number;
-  public hash: string;
-  public previousHash: string;
-  public data: string;
-  public timestamp: number;
-
   static calculaterBlockHash = (
     index: number,
     previousHash: string,
     timestamp: number,
     data: string
   ): string => CryptoJS.SHA256(index + previousHash + timestamp + data) + "";
+
+  // 들어온 블록의 구조가 유효한지 아닌지 판단
+  static validateStructure = (aBlock: Block): boolean =>
+    typeof aBlock.index === "number" &&
+    typeof aBlock.hash === "string" &&
+    typeof aBlock.previousHash === "string" &&
+    typeof aBlock.timestamp === "number" &&
+    typeof aBlock.data === "string";
+
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+
   constructor(
     index: number,
     hash: string,
@@ -38,6 +47,49 @@ const getLatestBlock = (): Block => blockChain[blockChain.length - 1];
 
 const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
-console.log(blockChain);
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLatestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimestamp: number = getNewTimeStamp();
+  const newHash: string = Block.calculaterBlockHash(
+    newIndex,
+    previousBlock.hash,
+    newTimestamp,
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previousBlock.hash,
+    data,
+    newTimestamp
+  );
+  return newBlock;
+};
+
+const getHashforBlock = (aBlock: Block): string =>
+  Block.calculaterBlockHash(
+    aBlock.index,
+    aBlock.previousHash,
+    aBlock.timestamp,
+    aBlock.data
+  );
+
+const isBlockBalid = (candidateBlock: Block, previousBlock: Block): boolean => {
+  // 블록의 구조가 유효한지 체크
+  if (!Block.validateStructure(candidateBlock)) {
+    return false;
+  } else if (previousBlock.index + 1 !== candidateBlock.index) {
+    return false;
+  } else if (previousBlock.hash !== candidateBlock.previousHash) {
+    return false;
+  } else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+console.log(createNewBlock("helllo"), createNewBlock("bye bye"));
 
 export {};
